b404d1c98dddc7e96f8bc465473672ba75e18a09
Carey <papaya0212@gmail.com>
2024-05-11 21:44:19 +0900

    rpn_calc lib 추가

---
+++ target/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["rpn_calc",{"doc":"#RPN Calc //! Reverse Polish notation (RPN) Calc.","t":"H","n":["eval"],"q":[[0,"rpn_calc"],[1,"alloc::string"],[2,"core::result"]],"d":[""],"i":[0],"f":"{b{{f{db}}}}","c":[],"p":[[5,"String",1],[1,"f64"],[6,"Result",2]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
